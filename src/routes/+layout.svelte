8503725c85743b80c0daf4719dd6e4b6c9e6801f
Saku <96244618+Sakkov@users.noreply.github.com>
2024-04-23 10:21:52 +0300

    added google tag manager tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,13 +16,21 @@
 </script>
 
 <svelte:head>
-	<!-- Google Tag Manager -->
+	<!-- Top Born? Google Tag Manager -->
 	<script>
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new
 Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='
 dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.
 parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-5RBHS6SZ');
 	</script>
+	<!-- End Google Tag Manager -->
+
+	<!-- Oma Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-MGXZZ8D8');</script>
 	<!-- End Google Tag Manager -->
 </svelte:head>
 
